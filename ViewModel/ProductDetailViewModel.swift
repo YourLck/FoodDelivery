f894d729cde8d405c3c0628f70030687d95250d9
YourLck <yourlck21i@gmail.com>
2023-05-31 22:59:12 +0300

    +Working with information in the cell

--- ViewModel/ProductDetailViewModel.swift
+++ ViewModel/ProductDetailViewModel.swift
@@ -9,15 +9,36 @@ import Foundation
 
 class ProductDetailViewModel: ObservableObject {
      
-   @Published  var product: ProductModel
-//    = ProductModel(id: "1",
-//                                                      title: "4Cheese",
-//                                                      imageUrl: "Not found",
-//                                                      price: 100,
-//                                                      weight: "450 g",
-//                                                      descript: "Composition: cheese, olives")
+    @Published var product: ProductModel
+    @Published var sizes = ["Small(20)", "Medium(30)", "Big(45)"]
+    @Published var weight = ["250 g", "500 g", "750 g"]
+    @Published var countProduct = 0
+
+    
     init(product: ProductModel) {
         self.product = product
+    }
+    
+    func selectionSize(size: String) -> Int {
+        switch size {
+        case "Small(20)" : return Int(Double(product.price) * 0.8)
+        case "Medium(30)" : return Int(product.price)
+        case "Big(45)" : return Int(Double(product.price) * 1.4)
+            
+        default:
+            return 0
+        }
+    }
+    
+    func selectionWeight(size: String) -> String {
+        switch size {
+        case "Small(20)" : return "250 g"
+        case "Medium(30)" : return "500 g"
+        case "Big(45)" : return "750 g"
+
+        default:
+            return ""
+        }
     }
     
 } 
--- Views/ProductDetailView.swift
+++ Views/ProductDetailView.swift
@@ -8,40 +8,70 @@
 import SwiftUI
 
 struct ProductDetailView: View {
+    @State var countProduct = 1
     
-    var viewModel: ProductDetailViewModel
-    
-    var body: some View {
+        var viewModel: ProductDetailViewModel
         
-        VStack(alignment: .leading) {
-            Image("pizza4Cheese")
-                .resizable()
-                .frame(maxWidth: .infinity, maxHeight: 260)
-            
-            HStack {
-                Text("\(viewModel.product.title)")
-                    .font(.title2.bold())
+        @State var size = "Medium(30)"
+        @State var weight = "500 g"
+        
+        var body: some View {
+            VStack {
+                
+                VStack(alignment: .leading) {
+                    Image("pizza4Cheese")
+                        .resizable()
+                        .frame(maxWidth: .infinity, maxHeight: 260)
+                    
+                    HStack {
+                        Text("\(viewModel.product.title)")
+                            .font(.title2.bold())
+                        Spacer()
+                        Text("\(viewModel.selectionSize(size: size )) $")
+                            .font(.title2)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Text("Weight: \(viewModel.selectionWeight(size: size))")
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 4)
+                        .font(.title2)
+                    
+                    Text("\(viewModel.product.descript)")
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 4)
+                        .font(.title2)
+                    //                Spacer()
+                    
+                    Stepper("Quantity", value: $countProduct, in: 1...10)
+                        .padding()
+                    
+                    Picker("Size" , selection: $size) {
+                        ForEach(viewModel.sizes, id: \.self ) { item in
+                            Text(item)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    .padding()
+                }
+                //        .ignoresSafeArea()
                 Spacer()
-                Text("\(viewModel.product.price) $")
-                    .font(.title2)
+                
+                Button {
+                    print("Add to cart!")
+                } label: {
+                    Text("Add to cart!")
+                        .padding()
+                        .padding(.horizontal, 20)
+                        .font(.title.bold())
+                        .background(Color("roseRed"))
+                        .foregroundColor(Color.black)
+                        .cornerRadius(20)
+
+                }
             }
-            .padding(.horizontal, 20)
-            
-            Text("Weight: \(viewModel.product.weight)")
-                .padding(.horizontal, 13)
-                .padding(.vertical, 4)
-                .font(.title2)
-            
-            Text("\(viewModel.product.descript)")
-                .padding(.horizontal, 13)
-                .padding(.vertical, 4)
-                .font(.title2)
-            
-                Spacer()
         }
-//        .ignoresSafeArea()
     }
-}
 
 struct ProductDetailView_Previews: PreviewProvider {
     static var previews: some View {
